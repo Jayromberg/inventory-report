b029c8fe2631984804e3dd391afad1b0c52fb011
Jayromberg Lima Santos <jayro@pop-os.localdomain>
2023-02-01 15:28:04 -0300

    test: test cria produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,29 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto = Product(
+        id=1,
+        nome_do_produto="Produto 1",
+        nome_da_empresa="Empresa 1",
+        data_de_fabricacao="2020-01-01",
+        data_de_validade="2020-12-31",
+        numero_de_serie=1,
+        instrucoes_de_armazenamento="Armazenamento 1",
+    )
+
+    assert produto.id == 1
+    assert produto.nome_do_produto == "Produto 1"
+    assert produto.nome_da_empresa == "Empresa 1"
+    assert produto.data_de_fabricacao == "2020-01-01"
+    assert produto.data_de_validade == "2020-12-31"
+    assert produto.numero_de_serie == 1
+    assert produto.instrucoes_de_armazenamento == "Armazenamento 1"
+
+    assert produto.__repr__() == (
+        f"O produto {produto.nome_do_produto}"
+        f" fabricado em {produto.data_de_fabricacao}"
+        f" por {produto.nome_da_empresa} com validade"
+        f" até {produto.data_de_validade}"
+        f" precisa ser armazenado {produto.instrucoes_de_armazenamento}."
+    )
