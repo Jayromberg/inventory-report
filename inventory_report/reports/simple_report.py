8c8105e5d57a54756c12f3ebb0237a504d890f9f
Jayromberg Lima Santos <jayro@pop-os.localdomain>
2023-02-02 12:11:00 -0300

    feat: Classe SimpleReport concluida

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,28 @@
+from datetime import date
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(products: list[dict]):
+        today = date.today().strftime("%Y/%m/%d")
+
+        oldest_manufacture = min(map(lambda p: (
+                p['data_de_fabricacao']
+            ), products))
+
+        closest_expiration = min(products, key=lambda p: (
+                p['data_de_validade']
+                if p['data_de_validade'] < today
+                else '9999/99/99'
+            ))['data_de_validade']
+
+        company_bigger_stock = Counter(map(lambda p: (
+                p['nome_da_empresa']
+            ), products)).most_common(1)[0][0]
+
+        return (
+            f"Data de fabricação mais antiga: {oldest_manufacture}\n"
+            f"Data de validade mais próxima: {closest_expiration}\n"
+            f"Empresa com mais produtos: {company_bigger_stock}"
+        )
