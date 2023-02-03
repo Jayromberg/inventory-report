92265a4709e09935076fbb7a954b616ce3a742f1
Jayromberg <jayro.dev@gmail.com>
2023-02-03 11:05:04 -0300

    feat: CompleteReport

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,25 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(products: list[dict]):
+        simple_report = SimpleReport.generate(products)
+
+        stock_by_company = Counter(map(lambda p: (
+            p['nome_da_empresa']
+        ), products)).most_common()
+
+        companies = []
+
+        for company, stock in stock_by_company:
+            companies.append(f"- {company}: {stock}")
+
+        return (
+            f"{simple_report}\n"
+            "Produtos estocados por empresa:\n"
+            f"{companies[0]}\n"
+            f"{companies[1]}\n"
+            f"{companies[2]}\n"
+        )
